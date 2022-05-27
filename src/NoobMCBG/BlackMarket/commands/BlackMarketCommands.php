7fc0446fb6a84eaca842ea2cc3939bbf10db90be
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-05-27 16:10:01 +0700

    Update BlackMarketCommands.php

--- src/NoobMCBG/BlackMarket/commands/BlackMarketCommands.php
+++ src/NoobMCBG/BlackMarket/commands/BlackMarketCommands.php
@@ -22,10 +22,40 @@ class BlackMarketCommands extends Command implements PluginOwned {
 
 	public function execute(CommandSender $sender, string $label, array $args){
         	if($sender instanceof Player){
-        		Forms::menuMarkets($sender);
+        		$this->menuMarkets($sender);
         	}else{
         		$this->plugin->getLogger()->error("Please use command in-game");
         	}
+	}
+	
+	public function menuMarkets(Player $player){
+		$form = new SimpleForm(function(Player $player, $data) {
+			if($data == null){
+				return true;
+			}
+			if($data == 0){
+				return true;
+			}
+			if($this->getOwningPlugin()->getMarket()->exists($data)){
+				if(!$player->hasPermission($this->getOwningPlugin()->getMarket()->get($data)["permission"])){
+					if($coin = \onebone\coinapi\CoinAPI::getInstance()->myCoin($player) >= $cost = $this->getOwningPlugin()->getMarket()->get($data)["cost"]){
+			    		$this->getOwningPlugin()->getServer()->getCommandMap()->dispatch(new \pocketmine\console\ConsoleCommandSender($this->getOwningPlugin()->getServer(), $instance->getServer()->getLanguage()), "setuperm ".$player->getName()." ".$instance->getMarket()->get($data)["permission"]);
+			    		$feature = str_replace(["", "", "", "", ""], ["§l", "§c", "§e", "•", " "], $this->getOwningPlugin()->getMarket()->get($data)["name"]);
+			    		$player->sendMessage("§l§c•§e Bạn Đã Mua Thành Công Tính Năng§a $feature");
+			    	}
+			    }else{
+			    	$player->sendMessage("§l§c•§e Bạn Đã Mua Tính Năng Này Trước Đó Rồi !");
+			    }
+			}
+		});
+        	$form->setTitle("§l§6♦§2 Chợ Đen §6♦");
+        	$form->addButton("§l§3•§2 Thoát Menu §3•");
+        	for($i = 1;$i <= 30;$i++){
+        		if($this->getOwningPlugin()->getMarket()->exists($i)){
+        	    		$form->addButton($this->getOwningPlugin()->getMarket()->get($i)["button"], 1, $this->getOwningPlugin()->getMarket()->get($i)["icon"]);
+        		}
+        	}
+        	$player->sendForm($form);
 	}
 
 	public function getOwningPlugin() : BlackMarket {
